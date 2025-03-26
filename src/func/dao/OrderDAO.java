1b0fe85d36dff8f853ff6799506044afee53182c
Quoc Tri <quocanh1204.dev@gmail.com>
2025-03-26 21:37:39 +0700

    Cập nhật tạo order

--- src/func/dao/OrderDAO.java
+++ src/func/dao/OrderDAO.java
@@ -39,19 +39,19 @@ public class OrderDAO extends FuncDAO<OrderEntity, String> {
             if (od.getOrderTableId() == 0) {
                 // Nếu không có order_table_id, bỏ cột này khỏi INSERT
                 sql = "DECLARE @InsertedTable TABLE (order_id INT); "
-                        + "INSERT INTO Orders (cashier_id, status) "
+                        + "INSERT INTO Orders (cashier_id, status, customer_phone, customer_name) "
                         + "OUTPUT INSERTED.order_id INTO @InsertedTable "
-                        + "VALUES (?, ?); "
+                        + "VALUES (?, ?,?,?); "
                         + "SELECT order_id FROM @InsertedTable;";
-                rs = Database.query(sql, od.getCashierId(), od.getStatus());
+                rs = Database.query(sql, od.getCashierId(), od.getStatus(), od.getCustomerPhone(), od.getCustomerName());
             } else {
                 // Nếu có order_table_id, insert đầy đủ
                 sql = "DECLARE @InsertedTable TABLE (order_id INT); "
-                        + "INSERT INTO Orders (cashier_id, status, order_table_id) "
+                        + "INSERT INTO Orders (cashier_id, status, order_table_id,customer_phone, customer_name) "
                         + "OUTPUT INSERTED.order_id INTO @InsertedTable "
-                        + "VALUES (?, ?, ?); "
+                        + "VALUES (?, ?, ?,?,?); "
                         + "SELECT order_id FROM @InsertedTable;";
-                rs = Database.query(sql, od.getCashierId(), od.getStatus(), od.getOrderTableId());
+                rs = Database.query(sql, od.getCashierId(), od.getStatus(), od.getOrderTableId(), od.getCustomerPhone(), od.getCustomerName());
             }
 
             if (rs.next()) {
