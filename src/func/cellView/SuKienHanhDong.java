965fb04af66bdc0c28d7b8564ac51d2efc4847bb
DucManh <thun21489@gmail.com>
2025-03-27 19:54:46 +0700

    Quan Ly Hoa Don

---
+++ src/func/cellView/SuKienHanhDong.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package func.cellView;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface SuKienHanhDong {
+    public void Edit(int row);
+    public void Delete(int row);
+    
+}
